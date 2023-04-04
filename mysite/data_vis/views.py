111520cf5ed6cea739ab86d009347c860e862533
davidayu <dyu22@calpoly.edu>
2023-04-03 21:32:42 -0700

    Added files

---
+++ mysite/data_vis/views.py
@@ -0,0 +1,28 @@
+from .models import AccountUser
+from django.http import HttpResponse
+from django.db import connection
+
+# http://127.0.0.1:8000/data_vis/user_list/72/
+def user_list(request, user_id=None):
+    with connection.cursor() as cursor:
+        if user_id:
+            cursor.execute("SELECT username, email FROM account_user WHERE id = %d" % (user_id))
+        else:
+            cursor.execute("SELECT username, email FROM account_user")
+        rows = cursor.fetchall()
+    return HttpResponse(rows)
+
+# http://127.0.0.1:8000/data_vis/question_text/36/
+def question_text(request, question_id):
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT text FROM questionnaire_question WHERE id = %d" % (question_id))
+        rows = cursor.fetchall()
+    return HttpResponse(rows)
+
+# http://127.0.0.1:8000/data_vis/questionnaire_user_answers/36/72/
+def questionnaire_user_answers(request, question_id, user_id):
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT timestamp, text_answer, slider_answer, numeric_answer FROM questionnaire_useranswer \
+            WHERE question_id = %d AND user_id = %d" % (question_id, user_id))
+        rows = cursor.fetchall()
+    return HttpResponse(rows)
