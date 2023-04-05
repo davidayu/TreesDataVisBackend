87cb9f75d7fa5a15b1b3ee4bbc6ebaf289ea4abb
davidayu <dyu22@calpoly.edu>
2023-04-05 01:49:45 -0700

    Modified files

--- data_vis_drf/data_vis_drf/settings.py
+++ data_vis_drf/data_vis_drf/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'data_vis_drf',
 
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'data_vis_drf.urls'
@@ -128,3 +130,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
