c98cb1af86249d907ad2d71bab359b7386214a7d
davidayu <dyu22@calpoly.edu>
2023-06-14 05:02:17 -0700

    Added README. Modified views.py to add comments

--- data_vis_drf/data_vis_drf/views.py
+++ data_vis_drf/data_vis_drf/views.py
@@ -7,6 +7,7 @@ from .serializers import AccountUserSerializer, QuestionnaireQuestionSerializer,
 from django.db.models import Q
 
 # http://127.0.0.1:8000/user_list/72/
+''' Gets all AccountUser in database. Specifying user_id will return only the user with that id '''
 @api_view(['GET'])
 def user_list(request, user_id=None):
     if user_id:
@@ -17,6 +18,7 @@ def user_list(request, user_id=None):
     return Response(serializer.data)
 
 # http://127.0.0.1:8000/question_text/36/
+''' Gets the text of a QuestionnaireQuestion with matching question_id '''
 @api_view(['GET'])
 def question_text(request, question_id):
     question = QuestionnaireQuestion.objects.get(id=question_id)
@@ -24,6 +26,7 @@ def question_text(request, question_id):
     return Response(serializer.data)
 
 # http://127.0.0.1:8000/questionnaire_user_answers/36/72/
+''' Gets all QuestionnaireUseranswers from user with user_id that answer QuestionnaireQuestion with question_id '''
 @api_view(['GET'])
 def questionnaire_user_answers(request, question_id, user_id):
     user_answers = QuestionnaireUseranswer.objects.filter(
@@ -35,6 +38,7 @@ def questionnaire_user_answers(request, question_id, user_id):
     return Response(serializer.data)
 
 # http://127.0.0.1:8000/path_feedback/123/
+''' Gets all PathUserpathfeedback for a particular path with path_id'''
 @api_view(['GET'])
 def path_user_pathfeedback(request, path):
     pathfeedback = PathUserpathfeedback.objects.filter(
